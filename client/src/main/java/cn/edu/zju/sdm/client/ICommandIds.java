5da4f525dd2b5b8f7b90a3f7bb3ff1dfb977afb9
hyggyh <hyggyh1990@gmail.com>
2014-07-21 22:41:36 +0800

    Upload the basic structure of the platform
    Signed-off-by: hyggyh <hyggyh1990@gmail.com>

---
+++ client/src/main/java/cn/edu/zju/sdm/client/ICommandIds.java
@@ -0,0 +1,15 @@
+package cn.edu.zju.sdm.client;
+
+/**
+ * Interface defining the application's command IDs.
+ * Key bindings can be defined for specific commands.
+ * To associate an action with a command, use IAction.setActionDefinitionId(commandId).
+ *
+ * @see org.eclipse.jface.action.IAction#setActionDefinitionId(String)
+ */
+public interface ICommandIds {
+
+    public static final String CMD_OPEN = "cn.edu.zju.sdm.client.open";
+    public static final String CMD_OPEN_MESSAGE = "cn.edu.zju.sdm.client.openMessage";
+    
+}
